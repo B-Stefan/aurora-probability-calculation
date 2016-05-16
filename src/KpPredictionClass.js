eb40243311d3ca800d0d4eb9faa8a75d67a96e6c
Stefan B <Stefan.bie@live.de>
2016-05-16 14:20:03 +0200

    - smr test

--- src/KpPredictionClass.js
+++ src/KpPredictionClass.js
@@ -1,5 +1,5 @@
 import {GeomagneticLatToKPIndex} from "geomagnatic-to-kp-index-converter"
-import svm from "node-svm"
+import smr from "smr"
 import so from "stringify-object"
 export default class KpPredictionClass {
 
@@ -7,30 +7,28 @@ export default class KpPredictionClass {
   constructor(){
 
     const options = {
-        gamma: [0.125, 0.5, 1],
-        c: [8, 16, 32],
-        epsilon: [0.001, 0.125, 0.5],
-        normalize: true, // (default)
-        reduce: true, // (default)
-        retainedVariance: 0.995,
-        kFold: 5
+        numX:2,
+        numY:1
       }
 
-    this.svm = new svm.EpsilonSVR(options);
+    this.smr =new smr.Regression(options)
 
   }
 
   train(referenceData){
-      console.log(referenceData)
-      const trainData = this.transformReferencData(referenceData)
+      const trainData = this.transformReferencData(referenceData);
 
       if(typeof this.trainPromise == "undefined"){
-        this.trainPromise = this.svm.train(trainData)
-            .progress((progress)=>{
-               console.log('training progress: %d%', Math.round(progress*100));
-            }).spread(function (model, report) {
-                console.log('SVM trained. \nReport :\n%s', so(report));
+        this.trainPromise = new Promise((resolve, reject)=>{
+
+            trainData.forEach((item)=>{
+              console.log(item)
+                this.smr.push(item)
             });
+
+            console.log(this.smr.calculateCoefficients());
+            resolve()
+        });
       }
       return this.trainPromise
 
@@ -47,7 +45,7 @@ export default class KpPredictionClass {
       */
       const transformed =  referenceData.map((town)=>{
           return town.values.map((townValues)=>{
-              return [[town.geoMagLat, townValues.kp],townValues.probability]
+              return {x: [town.geoMagLat, townValues.kp], y: [townValues.probability]}
           })
       })
       //flattern
@@ -62,6 +60,6 @@ export default class KpPredictionClass {
   */
   predict(gLat, kpIndex){
 
-    return this.svm.predictSync([gLat,kpIndex]);
+    return this.smr.hypothesize({x: [gLat,kpIndex]})[0];
   }
 }
